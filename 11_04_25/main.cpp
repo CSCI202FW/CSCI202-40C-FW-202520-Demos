2b911fa03bebf4ec1b5bda2ea46c65eae9cfe743
Angela Venable <avenable11@ivytech.edu>
2025-11-04 15:51:48 -0500

    first regex program

---
+++ 11_04_25/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <regex>
+#include <format>
+#include "date.h"
+
+int main()
+{
+    std::regex dateRegex{R"((\d{4})(\/|-)(1[0-2]|0?[1-9])(\2)([1-2][0-9]|3[0-1]|0?[1-9]))"};
+    while (true)
+    {
+        std::cout << "Enter a date (year/month/day) (q=quit): ";
+        std::string str;
+        if (!getline(std::cin, str) || str == "q")
+        {
+            break;
+        }
+        std::smatch m;
+        if (std::regex_match(str, m, dateRegex))
+        {
+            date myDate(stoi(m[3]), stoi(m[5]), stoi(m[1]));
+            m[1];
+            m[2];
+            m[3];
+
+            std::cout << "  Valid Date." << std::endl;
+        }
+        else
+        {
+            std::cout << "  Invalid Date!" << std::endl;
+        }
+    }
+    return 0;
+}
