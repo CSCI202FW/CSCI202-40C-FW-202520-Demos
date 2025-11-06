22bff45819bd7d257906650e74b9e3961224db0b
Angela Venable <avenable11@ivytech.edu>
2025-11-06 15:39:08 -0500

    copying

--- 11_06_25/linkedList.h
+++ 11_06_25/linkedList.h
@@ -3,5 +3,101 @@
 
 #include <iostream>
 #include <stdexcept>
+#include "node.h"
+template <class t>
+class linkedListType
+{
+public:
+    linkedListType();
+    linkedListType(const linkedListType<t> &otherList);
+    const linkedListType<t> &operator=(const linkedListType<t> &);
+    ~linkedListType();
+    void destroyList();
+    bool isEmptyList() const;
+    int length() const;
+    t front() const;
+    t back() const;
+    /* virtual void insert(const t &newInfo) = 0;
+    virtual void deleteNode(const t &deleteItem) = 0;
+    virtual bool search(const t &searchItem) = 0; */
+    void print(std::ostream &, std::string = " ") const;
+
+protected:
+    node<t> *head;
+    node<t> *tail;
+    int count;
+
+private:
+    void copyList(const linkedListType<t> &otherList);
+};
+
+template <class t>
+linkedListType<t>::linkedListType()
+{
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+}
+
+template <class t>
+linkedListType<t>::linkedListType(const linkedListType<t> &otherList)
+{
+    head = nullptr;
+    tail = nullptr;
+    count = 0;
+    copyList(otherList);
+}
+
+template <class t>
+const linkedListType<t> &linkedListType<t>::operator=(const linkedListType<t> &otherList)
+{
+    if (this != &otherList)
+    {
+        copyList(otherList);
+    }
+    return *this;
+}
+
+template <class t>
+void linkedListType<t>::copyList(const linkedListType<t> &fromList)
+{
+    node<t> *newNode;
+    node<t> *current;
+    if (!isEmptyList())
+    {
+        destroyList();
+    }
+    if (fromList.isEmptyList())
+    {
+        head = nullptr;
+        tail = nullptr;
+        count = 0;
+    }
+    else
+    {
+        current = fromList.head;
+        count = fromList.count;
+        this->head = new node<t>;
+        // don't do this shallow copy this->head->data = fromList.head->data;
+        this->head->data = new t(*(fromList.head->data));
+        this->head->link = nullptr;
+        this->tail = this->head;
+        current = current->link;
+        while (current != nullptr)
+        {
+            newNode = new node<t>;
+            nodeNode->data = new t(*(current->data));
+            newNode->link = nullptr;
+            tail->link = newNode;
+            tail = tail->link;
+            current = current->link;
+        }
+    }
+}
+
+template <class t>
+linkedListType<t>::~linkedListType()
+{
+}
 
 #endif
--- 11_06_25/main.cpp
+++ 11_06_25/main.cpp
@@ -1,7 +1,13 @@
 #include "donut.h"
+#include "linkedList.h"
 
 int main()
 {
+    linkedListType<int> myList;
+    int x = 7;
+    x = x;
+    myList = myList;
+    linkedListType<int> yourList = myList;
 
     return 0;
 }
