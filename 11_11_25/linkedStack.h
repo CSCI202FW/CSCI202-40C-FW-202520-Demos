5f267a230685f4e31a3d06d8bebe66429ede472e
Angela Venable <avenable11@ivytech.edu>
2025-11-11 16:47:17 -0500

    binary number conversion using stacks

--- 11_11_25/linkedStack.h
+++ 11_11_25/linkedStack.h
@@ -35,9 +35,63 @@ linkedStack<t>::linkedStack(int)
 }
 
 template <class t>
+linkedStack<t>::linkedStack(const linkedStack<t> &stackToCopy)
+{
+    stackTop = nullptr;
+    count = 0;
+    copyStack(stackToCopy);
+}
+
+template <class t>
+const linkedStack<t> &linkedStack<t>::operator=(const linkedStack<t> &stackToCopy)
+{
+    if (this != &stackToCopy)
+    {
+        copyStack(stackToCopy);
+    }
+    return *this;
+}
+
+template <class t>
 linkedStack<t>::~linkedStack()
 {
     initializeStack();
+}
+
+template <class t>
+void linkedStack<t>::copyStack(const linkedStack<t> &stackToCopy)
+{
+    node<t> *newNode;
+    node<t> *current;
+    node<t> *last;
+    if (!isEmptyStack())
+    {
+        initializeStack();
+    }
+    if (stackToCopy.isEmptyStack())
+    {
+        this->stackTop = nullptr;
+        count = 0;
+    }
+    else
+    {
+        current = stackToCopy.stackTop;
+        this->stackTop = new node<t>;
+        this->stackTop->data = new t(*(current->data));
+        this->stackTop->link = nullptr;
+        last = this->stackTop;
+        current = current->link;
+        while (current != nullptr)
+        {
+            newNode = new node<t>;
+            newNode->data = new t(*(current->data));
+            newNode->link = nullptr;
+            last->link = newNode;
+            last = newNode;
+            current = current->link;
+        }
+        this->count = stackToCopy.count;
+    }
 }
 
 template <class t>
@@ -45,7 +99,7 @@ t linkedStack<t>::getTopItem() const
 {
     if (isEmptyStack())
     {
-        throw std::out_of_range("Empty Stack")
+        throw std::out_of_range("Empty Stack");
     }
     return *(stackTop->data);
 }
@@ -75,6 +129,16 @@ bool linkedStack<t>::isEmptyStack() const
     return stackTop == nullptr;
 }
 template <class t>
+void linkedStack<t>::push(const t &newItem)
+{
+    node<t> *newNode;
+    newNode = new node<t>;
+    newNode->data = new t(newItem);
+    newNode->link = stackTop;
+    stackTop = newNode;
+    count++;
+}
+template <class t>
 t linkedStack<t>::peek() const
 {
     return getTopItem();
@@ -83,5 +147,20 @@ template <class t>
 t linkedStack<t>::top() const
 {
     return getTopItem();
+}
+template <class t>
+t linkedStack<t>::pop()
+{
+    node<t> *temp;
+    if (isEmptyStack())
+    {
+        throw std::underflow_error("Stack underflow. Cannot remove from an empty stack");
+    }
+    t copy = getTopItem();
+    temp = stackTop;
+    stackTop = stackTop->link;
+    delete temp;
+    count--;
+    return copy;
 }
 #endif
--- 11_11_25/main.cpp
+++ 11_11_25/main.cpp
@@ -1,12 +1,54 @@
 #include "arrayStack.h"
+#include "linkedStack.h"
+#include <iostream>
+#include <string>
+#include <cmath>
 
+// m03 lab a which stack to you prefer why?
+
+std::string getBinaryNumber();
 int main()
 {
-    arrayStack<int> myStack(20);
+    linkedStack<int> myStack(20);
     myStack.push(7);
     myStack.push(12);
-    arrayStack<int> yourStack = myStack;
+    linkedStack<int> yourStack = myStack;
     myStack.push(34);
     yourStack = myStack;
+
+    std::string binary = getBinaryNumber();
+    linkedStack<int> digits;
+    for (int i = 0; i < binary.size(); i++)
+    {
+        if (binary[i] != ' ')
+        {
+            digits.push(binary[i] - '0');
+        }
+    }
+    int position = 0;
+    int decimalNum = 0;
+    while (!digits.isEmptyStack())
+    {
+        int digit = digits.pop();
+        decimalNum += pow(2, position) * digit;
+        position++;
+    }
+    std::cout << binary << " in decimal is " << decimalNum << std::endl;
     return 0;
+}
+
+std::string getBinaryNumber()
+{
+    std::string binNumber;
+    std::cout << "Enter a number in binary: ";
+    std::getline(std::cin >> std::ws, binNumber);
+    for (int i = 0; i < binNumber.size(); i++)
+    {
+        if (binNumber[i] != '1' && binNumber[i] != '0' && binNumber[i] != ' ')
+        {
+            std::cout << "Please enter only 1s and 0s." << std::endl;
+            return getBinaryNumber();
+        }
+    }
+    return binNumber;
 }
