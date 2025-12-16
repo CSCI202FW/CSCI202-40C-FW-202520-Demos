89a4ddda0e4a73001174277502b324b8b1c5ca00
Angela Venable <avenable11@ivytech.edu>
2025-12-16 16:41:13 -0500

    final blockchain with sha256

--- 12_16_25/block.h
+++ 12_16_25/block.h
@@ -4,23 +4,34 @@
 #include <string>
 #include <ctime>
 #include <functional>
+#include <openssl/sha.h>
 
 class block
 {
 public:
-    block(int, std::string, size_t = 0);
+    // block(int, std::string, size_t = 0);
+    block(int, std::string, const unsigned char *);
+    block(const block &);
+    // const block &operator=(const block &);
     std::string getData() { return data; };
-    size_t getHash() { return currentHash; };
+    // size_t getHash() { return currentHash; };
+    const unsigned char *getHash() { return currentHash; };
     bool validateHash();
-    size_t getPrevHash() { return prevHash; };
+    // size_t getPrevHash() { return prevHash; };
+    const unsigned char *getPrevHash() { return prevHash; };
+    ~block();
 
 private:
     std::string data;
     time_t nonce;
-    size_t prevHash;
-    size_t currentHash;
+    // size_t prevHash;
+    // size_t currentHash;
+    unsigned char prevHash[SHA256_DIGEST_LENGTH];
+    unsigned char *currentHash;
     int blockNum;
-    size_t calculateHash();
+    // size_t calculateHash();
+    unsigned char *calculateHash();
+    void copyblock(const block &);
 };
 
 #endif
--- 12_16_25/blockChain.cpp
+++ 12_16_25/blockChain.cpp
@@ -2,12 +2,51 @@
 
 blockChain::blockChain()
 {
+    numBlocks = 0;
+    // block genesis(numBlocks++, "Genesis Block");
+    unsigned char hash[SHA256_DIGEST_LENGTH] = {0};
+    block genesis(numBlocks++, "Genesis Block", hash);
+    chain.enqueue(genesis);
 }
 
-void blockChain::insert(std::string)
+void blockChain::insert(std::string message)
 {
+    block newBlock(numBlocks++, message, chain.back().getHash());
+    chain.enqueue(newBlock);
 }
 
 void blockChain::verify()
 {
+    linkedQueue<block> cpy(chain);
+    // size_t prevHash = 0;
+    unsigned char *prevHash = nullptr;
+    while (!cpy.isEmptyQueue())
+    {
+        block current(cpy.dequeue());
+        if (!current.validateHash())
+        {
+            throw std::runtime_error("Block has been changed");
+        }
+        const unsigned char *currentPrevHash = current.getPrevHash();
+        // size_t currentPrevHash = current.getPrevHash();
+        // if (prevHash != 0)
+        if (prevHash != nullptr)
+        {
+            for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+            {
+                // if (prevHash != currentPrevHash)
+                if (prevHash[i] != currentPrevHash[i])
+                {
+                    throw std::runtime_error("Chain has been changed");
+                }
+            }
+        }
+        delete prevHash;
+        const unsigned char *currentHash = current.getHash();
+        prevHash = new unsigned char[SHA256_DIGEST_LENGTH];
+        for (int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+        {
+            prevHash[i] = currentHash[i];
+        }
+    }
 }
